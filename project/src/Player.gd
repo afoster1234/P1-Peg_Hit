9f5ed3a7ece7f8ba3805b0fedb197af53c42016c
Austin Foster <afoster@bsu.edu>
2020-09-06 18:32:57 -0400

    Major Rehaul

--- project/src/Player.gd
+++ project/src/Player.gd
@@ -1,24 +1,25 @@
-extends Node2D
+extends RigidBody2D
 
-export var speed = 200
 var screen_size
+export var strength : float = 200
 
 func _ready():
 	screen_size = get_viewport_rect().size
 
 func _process(delta):
-	var velocity = Vector2()
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-	position += velocity * delta
+	var speed = Vector2()
+	if Input.is_action_just_pressed("right"):
+		var direction = Vector2(1,-1).normalized()
+		var velocity = direction * strength
+		apply_impulse(Vector2.ZERO, velocity)
+	if Input.is_action_just_pressed("left"):
+		var direction = Vector2(-1,1).normalized()
+		var velocity = direction * strength
+		apply_impulse(Vector2.ZERO, velocity)
+	if Input.is_action_just_pressed("up"):
+		var direction = Vector2(10,0).normalized()
+		var velocity = direction * strength
+		apply_impulse(Vector2.ZERO, velocity)
+	position += speed * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
-	
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
