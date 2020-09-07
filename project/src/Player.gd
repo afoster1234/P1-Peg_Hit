9331994f18b97d2742c06ddeb53e9d8f51102532
Austin Foster <afoster@bsu.edu>
2020-09-06 20:07:31 -0400

    Functional Game

--- project/src/Player.gd
+++ project/src/Player.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+var speed_settings 
 var bullet_fired = preload("res://src/Bullet.tscn")
 
 func _physics_process(delta):
@@ -8,4 +9,4 @@ func _physics_process(delta):
 		var bullet = bullet_fired.instance()
 		get_tree().get_root().add_child(bullet)
 		bullet.position = get_global_position()
-		bullet.fire(get_global_mouse_position(),10)
+		bullet.fire(get_global_mouse_position(),5)
